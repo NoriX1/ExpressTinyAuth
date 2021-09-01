d4cb59c3763f09cb3271b1937b193dee54488e36
NoriX <versatility.95@mail.ru>
2021-09-01 17:20:16 +0300

    All controllers refactored with decorators

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 import { get, controller, bodyValidator, post } from "./decorators";
 
 @controller("/auth")
@@ -31,5 +31,11 @@ class LoginController {
     } else {
       res.send("Invalid email or password");
     }
+  }
+
+  @get("/logout")
+  getLogout(req: Request, res: Response) {
+    req.session = undefined;
+    res.redirect("/");
   }
 }
